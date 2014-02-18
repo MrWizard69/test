3b9c96ae1559645ad8da5380c6ca6e3c5fac6d45
Jordan Wilson <JordanWilson@fullsail.edu>
2014-02-17 23:55:25 -0500

    "Git" err dun!

---
+++ Week2/Wilson_Jordan_Week2_Assignment/js/tabs.js
@@ -0,0 +1,11 @@
+//login
+ $(document).ready(function(){
+			$(".box").mouseenter(function(){
+				$(".loginbox").slideDown(400);
+			});
+
+			$(".loginbox").mouseleave(function(){
+				$(".loginbox").slideUp(400);
+			});
+		
+		});
