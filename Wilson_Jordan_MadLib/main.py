0719e6cdb4db5dd8c95be257bf8a32ddaa66df31
Jordan Wilson <JordanWilson@fullsail.edu>
2014-05-05 22:29:36 -0400

    commit1

---
+++ Wilson_Jordan_MadLib/main.py
@@ -0,0 +1,15 @@
+'''
+Multi-line comments
+Jordan Wilson
+5/5/14
+DPW
+'''
+
+name1 = raw_input('Who is the first person?')
+val1 = raw_input('How much money do they have?')
+name2 = raw_input('Who is the second person?')
+val2 = raw_input('How much money do they have?')
+name3 = raw_input('Who is the third person?')
+val3 = raw_input('How much money do they have?')
+
+people = [name1, val1, name2, val2, name3, val3]
