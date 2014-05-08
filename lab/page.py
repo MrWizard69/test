4a54e5d5481a29891a193d9877627448cdde3f75
Jordan Wilson <JordanWilson@fullsail.edu>
2014-05-08 17:21:15 -0400

    commit

---
+++ lab/page.py
@@ -0,0 +1,38 @@
+class HTMLPage(object):
+    def __init__(self): #constructor
+        self.page_open = '''
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Welcome to my page</title>
+        <link href="css/styles.css" rel="stylesheet" type="text/css"
+    </head>
+    <body>
+        '''
+        self.page_content = '''
+        <h1>Pick Up Your New Car Today!</h1>
+    <form method="GET" action="">
+        <input type="text" name="firstname" placeholder="First Name" />
+        <input type="text" name="lastname" placeholder="Last Name" />
+        <input type="text" name="cardnumber" placeholder="Credit Card Number" />
+        <input type="checkbox" name="resident" value="FL Resident">Are you a florida resident?
+
+        <select name="car">
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="mercedes">Mercedes</option>
+            <option value="audi">Audi</option>
+        </select>
+
+        <input type="submit" name="submit" value="go" />
+    </form>
+        '''
+        self.page_close = '''
+    </body>
+</html>
+'''
+    def print_out(self, content):
+        #if the content.. is an empty string.. print the form
+        #else.. print out the content
+        return self.page_open + self.page_content + content + self.page_close
