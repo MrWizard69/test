b796604f0305afb32b568d8a460e7ce15931ffd0
Jordan Wilson <JordanWilson@fullsail.edu>
2014-05-07 22:08:53 -0400

    quiz2

--- basic_Python_Syntax_Quiz/main.py
+++ basic_Python_Syntax_Quiz/main.py
@@ -9,3 +9,10 @@ def calc_area():
     return area
 
 print calc_area()
+
+beer = 99
+def count_down(beer):
+    for i in range(beer, 1, -1):
+        print str(i) +" bottles of beer on the wall!"
+        print "Now you have " + str(i - 1) + " bottles of beer on the wall!"
+print count_down(beer)
