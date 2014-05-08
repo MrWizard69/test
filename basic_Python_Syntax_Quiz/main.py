a4184faaf74fdcd3d194edd9fc4628b4d73eff22
Jordan Wilson <JordanWilson@fullsail.edu>
2014-05-07 22:21:12 -0400

    quiz6

--- basic_Python_Syntax_Quiz/main.py
+++ basic_Python_Syntax_Quiz/main.py
@@ -1,6 +1,8 @@
+width = int(raw_input("What is the width? "))
+height = int(raw_input("What is the height? "))
+
 def calc_area(): #this is calculate area and tell the user if they are making a square or rectangle
-    width = 10
-    height = 10
+
     area = width * height
     if width == height:
         print "This is a square"
@@ -10,7 +12,7 @@ def calc_area(): #this is calculate area and tell the user if they are making a 
 
 print calc_area() #this is where it's spit out
 
-beer = 99
+beer = int(raw_input("How much beer is on the wall? "))
 def count_down(beer): # this will play the beer song
     for i in range(beer, 1, -1):
         print str(i) +" bottles of beer on the wall!" #this is where the beers number is displayed
