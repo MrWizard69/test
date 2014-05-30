1f8c0123a2748d221a551bb96a3f0c5804054b8b
Jordan Wilson <JordanWilson@fullsail.edu>
2014-05-30 00:42:40 -0400

    api9

--- wilson-jordan-api/main.py
+++ wilson-jordan-api/main.py
@@ -59,8 +59,8 @@ class nytView(object):
              <div class="wrapper">
              <h1>''' + self.articles[news].title + '''</h1>
              <p>''' + self.articles[news].abstract + '''</p>
-             <img src="''' + self.articles[news].pic + '''" />
              <a href="''' + self.articles[news].link + '''">Read More...</a>
+             <img src="''' + self.articles[news].pic + '''" />
              </div>
             '''
         # self.content = self.content.format(**locals())
