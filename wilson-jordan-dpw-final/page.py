cbd3fc8950d4902440c6f9f5dec29033704221ee
Jordan Wilson <JordanWilson@fullsail.edu>
2014-05-30 20:00:20 -0400

    final3

--- wilson-jordan-dpw-final/page.py
+++ wilson-jordan-dpw-final/page.py
@@ -1,7 +1,7 @@
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         p = FormPage()
-        self.response.write(print_out())
+        self.response.write(p.print_out())
 
 class Page(object):
     def __init__(self):
@@ -26,4 +26,16 @@ class Page(object):
     def update(self):
         self.all = self.open + self._content + self._close
         self.all = self.all.format(**locals())
+
+class FormPage(Page):
+    def __init__(self):
+        Page.__init__(self)
+        self.__form_open = '<form method="GET" action="">'
+        self.__inputs = '''
+        <input type="text" name="code" placeholder="placeholder" />
+        <input type="submit" name="submit" />'''
+        self.__form_close = '</form>'
+        self.form_header = 'string'
+        self.page_content = ''
+        self._content = self.form_header + self.__form_open +self.__inputs + self.__form_close
 
