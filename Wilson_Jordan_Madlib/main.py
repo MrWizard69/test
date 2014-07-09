e441a331c9e9813c77e6f07be7617371c7452ffa
Jordan Wilson <JordanWilson@fullsail.edu>
2014-07-08 21:54:23 -0400

    madlib2

--- Wilson_Jordan_Madlib/main.py
+++ Wilson_Jordan_Madlib/main.py
@@ -1,3 +1,11 @@
 #Jordan Wilson
 #07/08/14
 #Madlib
+
+noun = raw_input("Input a noun: ")
+verb = raw_input("Input a verb: ")
+num1 = raw_input("Input a number between 1-3: ")
+if int(num1) > 3 or int(num1) < 1:
+    print "Your number is not within the proper range"
+    num1 = input("Input a number between 1-3: ")
+
