7952ebbe790accf40333eac33051dadf1590963d
Jordan Wilson <JordanWilson@fullsail.edu>
2014-07-10 22:33:17 -0400

    quiz6

--- Wilson_Jordan_PythonQuiz1/main.py
+++ Wilson_Jordan_PythonQuiz1/main.py
@@ -1,20 +1,20 @@
-width = raw_input('Enter the width: ')
-height = raw_input('Enter the height: ')
+width = raw_input('Enter the width: ') #collecting users width
+height = raw_input('Enter the height: ') #colecting users height
 
-def calc_area(p1,p2):
+def calc_area(p1,p2): #the function that calcs the area
     area = p1 * p2
     if p1 == p2:
-        print 'It is a square!'
+        print 'It is a square!' #if the width and height is == then it's a square
     else:
-        print 'It is a rectangle!'
+        print 'It is a rectangle!' #if the width and height is != then it's a rectangle
     return area
 
-print calc_area(int(width),int(height))
+print calc_area(int(width),int(height)) #displaying the area
 
-booze = raw_input('How many bottles are on the wall? ')
+booze = raw_input('How many bottles are on the wall? ') #ALL the booze
 def count_down(booze):
-    for i in range(int(booze), 0, -1):
-        print str(i) + ' bottles of beer on the wall'
-        print 'take one down pass it around, now you have ' + str(i - 1) + ' bottles of beeeeeer!'
+    for i in range(int(booze), 0, -1): #the count down
+        print str(i) + ' bottles of beer on the wall' #number of beer
+        print 'take one down pass it around, now you have ' + str(i - 1) + ' bottles of beeeeeer!' #number of beer - 1
 
-print count_down(booze)
+print count_down(booze) #printing out the loop
