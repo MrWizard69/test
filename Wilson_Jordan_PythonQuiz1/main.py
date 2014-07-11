a9ec614cb06155c8e4da40c705887ed9496da907
Jordan Wilson <JordanWilson@fullsail.edu>
2014-07-10 22:12:07 -0400

    quiz2

--- Wilson_Jordan_PythonQuiz1/main.py
+++ Wilson_Jordan_PythonQuiz1/main.py
@@ -4,5 +4,7 @@ height = raw_input('Enter the height')
 def calc_area(p1,p2):
     area = p1 * p2
     if p1 == p2:
-        print 'It is a square'
-    
+        print 'It is a square!'
+    else:
+        print 'It is a rectangle!'
+    return area
