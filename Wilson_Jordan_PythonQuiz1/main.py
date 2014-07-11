edc023049cb7417a5eca34f9fcd32666062c9910
Jordan Wilson <JordanWilson@fullsail.edu>
2014-07-10 22:30:05 -0400

    quiz5

--- Wilson_Jordan_PythonQuiz1/main.py
+++ Wilson_Jordan_PythonQuiz1/main.py
@@ -12,7 +12,9 @@ def calc_area(p1,p2):
 print calc_area(int(width),int(height))
 
 booze = raw_input('How many bottles are on the wall? ')
+def count_down(booze):
+    for i in range(int(booze), 0, -1):
+        print str(i) + ' bottles of beer on the wall'
+        print 'take one down pass it around, now you have ' + str(i - 1) + ' bottles of beeeeeer!'
 
-for i in range(int(booze), 1, -1):
-    print str(i) + ' bottles of beer on the wall'
-    print 'take one down pass it around, now you have' + str(i - 1) + ' of beeeeeer!'
+print count_down(booze)
