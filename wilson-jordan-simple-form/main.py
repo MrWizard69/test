6e36df5c035833eccd4882bc3a074fc0756edc0c
Jordan Wilson <JordanWilson@fullsail.edu>
2014-07-14 16:16:48 -0400

    simple form11

--- wilson-jordan-simple-form/main.py
+++ wilson-jordan-simple-form/main.py
@@ -4,7 +4,8 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         p = Page()
         if self.request.GET:
-            p.content = "<p>Please keep this information for safe keeping.</p>"
+            p.content = "<h1>Thank You For Signing Up!</h1>"
+            p.content += "<p>Please keep this information for safe keeping.</p>"
             p.content += "<p>Full Name: " + self.request.GET['flname'] + "</p>"
             p.content += "<p>Username: " + self.request.GET['user'] + "</p>"
             p.content += "<p>Password: " + self.request.GET['pass'] + "</p>"
@@ -38,6 +39,12 @@ class Page(object):
             <p>Are you a Male?</p>
              <input type="checkbox" name="gender" value="yes" /> Yes
              <input type="checkbox" name="gender" value="No" /> No
+             <p>Now for sheets and giggles, choose your favorite console</p>
+             <select name="console">
+                <option value="XBox">XBox</option>
+                <option value="Playstation">Playstation</option>
+                <option value="Nintendo">Nintendo</option>
+            </select>
             <input type="submit" value="Submit Info" />
         </form>
     '''
