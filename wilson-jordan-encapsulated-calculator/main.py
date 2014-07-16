d89b425b2533faa4ea0f02a2b35a39935ff379e0
Jordan Wilson <JordanWilson@fullsail.edu>
2014-07-15 22:01:57 -0400

    encap calc4

--- wilson-jordan-encapsulated-calculator/main.py
+++ wilson-jordan-encapsulated-calculator/main.py
@@ -1,8 +1,17 @@
 import webapp2
+from page import Page
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        p = Page()
+
+class Games(object):
+    def __init__(self):
+        self.__title = ""
+        self.__price = 0
+        self.__rating = ""
+        self.__genre = ""
+        self.__synops = ""
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
