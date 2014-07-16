6232c86e538ba9f81078dd66b59604636739e050
Jordan Wilson <JordanWilson@fullsail.edu>
2014-07-15 21:44:11 -0400

    encap calc2

---
+++ wilson-jordan-encapsulated-calculator/page.py
@@ -0,0 +1,17 @@
+class Page():
+    def __init__(self):
+        self.open = '''
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Lab 3</title>
+    </head>
+    <body>
+        '''
+        self.content = '''
+            Content
+            '''
+        self.close = '''
+    </body>
+</html>
+        '''
