365e85c019ae73a8e0096af24987b3f301992de0
Jordan Wilson <JordanWilson@fullsail.edu>
2014-07-22 22:09:51 -0400

    quiz inheritance2- made the abstract class and sub classes

--- wilson-jordan-inheritance-quiz/main.py
+++ wilson-jordan-inheritance-quiz/main.py
@@ -22,6 +22,41 @@ class Page(object):
     def print_out(self):
         return self.header + self.content + self.closer
 
+class AbCar(object):
+    def __init__(self):
+        self.name = ''
+        self.__year = 0
+        self.color = ''
+    @property
+    def year(self):
+        return self.__year
+
+    @year.setter
+    def year(self, y):
+        self.__year = y
+
+class Car1(AbCar):
+    def __init__(self):
+        super(Car1, self).__init__()
+        self.name = "Shelby"
+        self.year = 1969
+        self.color = "Red"
+
+class Car2(AbCar):
+    def __init__(self):
+        super(Car2, self).__init__()
+        self.name = "Thomas"
+        self.year = 2003
+        self.color = "Yellow"
+
+class Car3(AbCar):
+    def __init__(self):
+        super(Car3, self).__init__()
+        self.name = "Missy"
+        self.year = "2014"
+        self.color = "Indigo"
+
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
