b819bb101f8ed15c93315e48676b3c51d75bd6cb
Jordan Wilson <JordanWilson@fullsail.edu>
2014-07-31 18:59:31 -0400

    final2- set up the object class and the Model

--- wilson-jordan-final/main.py
+++ wilson-jordan-final/main.py
@@ -33,7 +33,39 @@ class Page(object):
         self.page = self.header + self.content + self.closer
         self.update = self.page.format(**locals())
 
-class ThronesObject(object)
+class ThronesObject(object):
+    def __init__(self):
+        self.name = ''
+        self.sigil = ''
+        self.motto = ''
+        self.color1 = ''
+        self.color2 = ''
+        self.head = ''
+        self.image = ''
+        self.house = []
+
+class ThronesModel(object):
+    def __init__(self):
+        self.house = []
+        self.tgo = ''
+
+    def send_request(self):
+        url = 'http://rebeccacarroll.com/api/got/got.xml'
+        req = urllib2.Request(url)
+        opener = urllib2.build_opener()
+        data = opener.open(req)
+        xmldoc = minidom.parse(data)
+
+        throne = xmldoc.getElementsByTagName('houses')
+
+        for item in throne:
+            house = ThronesObject()
+
+            try:
+                house.name = item.getElementsByTagName('name')[0].firstChild.nodeValue
+            except:
+                house.name = 'No Name'
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
