57a7763028ff4f2dedbd86ab2135291402ed8640
Jordan Wilson <JordanWilson@fullsail.edu>
2014-07-31 22:08:39 -0400

    final4- got it working just have to set up the array

--- wilson-jordan-final/main.py
+++ wilson-jordan-final/main.py
@@ -5,25 +5,66 @@ import urllib2
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         p = Page()
+        p.title = "Final"
+        p.h1 = "God help us all..."
+
+        code = self.request.GET['code']
+
+
+        if self.request.GET:
+            g_model = ThronesModel()
+            g_model.houses = code
+            g_model.send_request()
+
+            g_view = ThronesView()
+            g_view.house = g_model.house
+            g_view.update()
+            p.content = g_view.content
 
         self.response.write(p.print_out())
 
 class Page(object):
     def __init__(self):
-        self. header = '''
+        self.header = '''
 <DOCTYPE HTML>
 <html>
 <head>
-    <title></title>
+    <title>{self.title}</title>
 </head>
 <body>
 '''
-        self.content = 'content'
+        self.content = '''
+        <h1>{self.h1}</h1>
+        <a href="?code=0">Link1</a>
+        <a href="?code=1">Link2</a>
+        <a href="?code=2">Link3</a>
+        <a href="?code=3">Link4</a>
+        <a href="?code=4">Link5</a>
+        <a href="?code=5">Link6</a>
+        '''
 
         self.closer = '''
 </body>
 </html>'''
         self.page = ''
+        self._title = ''
+        self.__h1 = ''
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, t):
+        self._title = t
+
+    @property
+    def h1(self):
+        return self.__h1
+
+    @h1.setter
+    def h1(self, h):
+        self.__h1 = h
 
     def print_out(self):
         self.update()
@@ -32,6 +73,8 @@ class Page(object):
     def update(self):
         self.page = self.header + self.content + self.closer
         self.update = self.page.format(**locals())
+
+
 
 class ThronesObject(object):
     def __init__(self):
@@ -48,55 +91,61 @@ class ThronesModel(object):
     def __init__(self):
         self.house = []
         self.tgo = ''
+        self.code = 'got.xml'
 
     def send_request(self):
-        url = 'http://rebeccacarroll.com/api/got/got.xml'
+        url = 'http://rebeccacarroll.com/api/got/' + self.code
         req = urllib2.Request(url)
         opener = urllib2.build_opener()
         data = opener.open(req)
         xmldoc = minidom.parse(data)
 
-        throne = xmldoc.getElementsByTagName('houses')
+        thrones = xmldoc.getElementsByTagName('house')
 
-        for item in throne:
-            house = ThronesObject()
+        #print 'test test test test!!!!!!!!!!'
 
-            try:
-                house.name = item.getElementsByTagName('name')[0].firstChild.nodeValue
-            except:
-                house.name = 'No Name'
+
+        for item in thrones:
+            throne = ThronesObject()
 
             try:
-                house.sigil = item.getElementsByTagName('sigil')[0].firstChild.nodeValue
+                throne.name = item.getElementsByTagName('name')[0].firstChild.nodeValue
             except:
-                house.sigil = 'No Sigil'
+                throne.name = 'No Name'
 
             try:
-                house.motto = item.getElementsByTagName('motto')[0].firstChild.nodeValue
+                throne.sigil = item.getElementsByTagName('sigil')[0].firstChild.nodeValue
             except:
-                house.motto = 'No Motto'
+                throne.sigil = 'No Sigil'
 
             try:
-                house.color1 = item.getElementsByTagName('color1')[0].firstChild.nodeValue
+                throne.motto = item.getElementsByTagName('motto')[0].firstChild.nodeValue
             except:
-                house.color1 = 'No Color'
+                throne.motto = 'No Motto'
 
             try:
-                house.color2 = item.getElementsByTagName('color2')[0].firstChild.nodeValue
+                throne.color1 = item.getElementsByTagName('color1')[0].firstChild.nodeValue
             except:
-                house.color2 = 'No Color'
+                throne.color1 = 'No Color'
 
             try:
-                house.head = item.getElementsByTagName('head')[0].firstChild.nodeValue
+                throne.color2 = item.getElementsByTagName('color2')[0].firstChild.nodeValue
             except:
-                house.head = 'No Head'
+                throne.color2 = 'No Color'
 
             try:
-                house.image = item.getElementsByTagName('image')[0].firstChild.nodeValue
+                throne.head = item.getElementsByTagName('head')[0].firstChild.nodeValue
             except:
-                house.image = ''
+                throne.head = 'No Head'
 
-        self.house.append(throne)
+            try:
+                throne.image = item.getElementsByTagName('image')[0].firstChild.nodeValue
+            except:
+                throne.image = ''
+
+            self.house.append(throne)
+
+
 
 class ThronesView(object):
     def __init__(self):
@@ -104,16 +153,18 @@ class ThronesView(object):
         self.content = ''
         self.house = []
 
+
+
+
     def update(self):
         for thrones in self.house:
             self.content += '''
-            <h1>''' + thrones.name + '''</h1>
-            <p>''' + thrones.sigil + '''</p>
-            <p>''' + thrones.motto + '''</p>
-            <p>''' + thrones.color1 + '''</p>
-            <p>''' + thrones.color2 + '''</p>
-            <p>''' + thrones.color1 + '''</p>
-            <p>''' + thrones.head + '''</p>
+            <h1>Name: ''' + thrones.name + '''</h1>
+            <p>Sigil: ''' + thrones.sigil + '''</p>
+            <p>Motto: ''' + thrones.motto + '''</p>
+            <p>Color1: ''' + thrones.color1 + '''</p>
+            <p>Color2: ''' + thrones.color2 + '''</p>
+            <p>Head: ''' + thrones.head + '''</p>
             <img src="''' + thrones.image + '''" />
             '''
 
